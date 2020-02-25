35c1d0da0c4e46b27ff8d243602d131bcb248676
Lucas Shanley <lpshanley@gmail.com>
2020-02-25 17:34:59 -0600

    Added some electron boilerplate to give a hybrid svelte/electron boilerplate.

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
 
 	export let name;
-
+	console.log(process)
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	
+	<p><strong>We are using</strong></p>
+	<p>Node {process.versions.node}</p>
+	<p>Chrome {process.versions.chrome}</p>
+	<p>Electron {process.versions.electron}</p>
 </main>
 
 <style>
